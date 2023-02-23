5488fa6c94cf9ba20f751ef644076ee584c255d5
randr97 <rohitshrothrium.ss1997@gmail.com>
2023-02-23 03:49:21 -0500

    [Testing][3.4] CPU Scheduling - handle_process_arrival_pp

--- cpu.c
+++ cpu.c
@@ -1,20 +1,52 @@
 #include <dirent.h> 
 #include <stdio.h> 
 
+#define QUEUEMAX 10
 
-int main(void) {
-  DIR *d;
-  struct dirent *dir;
-  d = opendir(".");
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
+
+struct PCB {
+    int process_id;
+    int arrival_timestamp;
+    int total_bursttime;
+    int execution_starttime;
+    int execution_endtime;
+    int remaining_bursttime;
+    int process_priority;
+};
+
+struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp) {
+    DIR *d;
+    struct dirent *dir;
+    struct PCB pcb1;
+    d = opendir(".");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
     }
-    closedir(d);
-  }
-  return(0);
+    return pcb1;
 }
-
+struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
+    struct PCB pcb1;
+    return pcb1;
+}
+struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int time_stamp) {
+    struct PCB pcb1;
+    return pcb1;
+}
+struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
+    struct PCB pcb1;
+    return pcb1;
+}
+struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum) {
+    struct PCB pcb1;
+    return pcb1;
+}
+struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp, int time_quantum) {
+    struct PCB pcb1;
+    return pcb1;
+}
 
 // #include <stdio.h>
 // #include <stdbool.h>
