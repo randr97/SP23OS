07c8b139be31fe4535beda3c86b88e8072c25d9b
randr97 <rohitshrothrium.ss1997@gmail.com>
2023-02-26 00:03:42 -0500

    [3.4] CPU Scheduling: all imp

--- cpu.c
+++ cpu.c
@@ -121,7 +121,7 @@ struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX],
     int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum) {
     if(isNullPCB(&current_process)) {
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + time_quantum < new_process.total_bursttime ? time_quantum: new_process.total_bursttime;
+        new_process.execution_endtime = timestamp + (time_quantum < new_process.total_bursttime ? time_quantum: new_process.total_bursttime);
         new_process.remaining_bursttime = new_process.total_bursttime;
         return new_process;
     } else {
