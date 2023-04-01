886a88f85597ab109383f759c52640847002b134
randr97 <rohitshrothrium.ss1997@gmail.com>
2023-03-31 20:06:07 -0400

    Mem allocation

--- virtual.c
+++ virtual.c
@@ -125,7 +125,59 @@ int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int ref
 }
 
 int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp) {
-    return 0;
+
+    // check if page is already in memory
+    if (page_table[page_number].is_valid == 1) {
+        // update last access time and reference count
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count++;
+        // return the frame number
+        return page_table[page_number].frame_number;
+    }
+    
+    // check if there are free frames
+    if (*frame_cnt > 0) {
+        // get a frame from the frame pool
+        int frame_number = frame_pool[(*frame_cnt)-1];
+        (*frame_cnt)--;
+        // update page table entry
+        page_table[page_number].is_valid = 1;
+        page_table[page_number].frame_number = frame_number;
+        page_table[page_number].arrival_timestamp = current_timestamp;
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count = 1;
+        // return the frame number
+        return frame_number;
+    }
+    
+    // no free frames, replace a page using LFU policy
+    int min_reference_count = INT_MAX;
+    int frame_to_replace = -1;
+    for (int i = 0; i < *table_cnt; i++) {
+        if (page_table[i].is_valid == 1 && page_table[i].reference_count < min_reference_count) {
+            min_reference_count = page_table[i].reference_count;
+            frame_to_replace = page_table[i].frame_number;
+        }
+    }
+    // update the page table entry of the page to be replaced
+    for (int i = 0; i < *table_cnt; i++) {
+        if (page_table[i].is_valid == 1 && page_table[i].frame_number == frame_to_replace) {
+            page_table[i].is_valid = 0;
+            page_table[i].frame_number = -1;
+            page_table[i].arrival_timestamp = -1;
+            page_table[i].last_access_timestamp = -1;
+            page_table[i].reference_count = -1;
+            break;
+        }
+    }
+    // update the page table entry of the newly referenced page
+    page_table[page_number].is_valid = 1;
+    page_table[page_number].frame_number = frame_to_replace;
+    page_table[page_number].arrival_timestamp = current_timestamp;
+    page_table[page_number].last_access_timestamp = current_timestamp;
+    page_table[page_number].reference_count = 1;
+    // return the frame number
+    return frame_to_replace;
 }
 
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt) {
